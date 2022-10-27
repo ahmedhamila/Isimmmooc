24f8810d54818912d3939ffa1a1398824d846758
Ghada <gh.abassi01@gmail.com>
2022-10-27 13:02:45 -0500

    Comite back 1

--- IsimmmoocBackend/Isimmmooc_Users/models/Comite.py
+++ IsimmmoocBackend/Isimmmooc_Users/models/Comite.py
@@ -1,15 +1,35 @@
+from unittest.util import _MAX_LENGTH
 from django.db import models
+from phonenumber_field.modelfields import PhoneNumberField
 
 
-class Comite (models.Model):
-     #TODO:les attributs approprie pour le model
-   name = models.CharField(max_length=20, help_text='Nom de Comite')
+# Create your models here.
 
-   #Metadata
-   class Meta :
-       ordering = ['-name']
+class Comite(models.Model):
+    nom=models.CharField(max_length=18,null=False,blank=True)
+    prenom=models.CharField(max_length=18,null=False,blank=True)
+    grade=models.CharField(max_length=12,null=False,blank=True)
+    departement=models.CharField(max_length=12,null=False,blank=True)
+    password=models.CharField(max_length=15,null=False,blank=True)
+    Email=models.EmailField(max_length=12,null=False,blank=True,unique=True)
+    phone=PhoneNumberField(max_length=8,null=False, blank=False,unique=True)
 
-   #Methods
+    class Meta :
+        constraints = [
+            models.CheckConstraint(
+                check=models.Q(phone__length__gte=8),
+                name="%(app_label)s_%(class)s_title_length",
+            )
+        
+    ]
+    ordering = ['-nom']
+    ordering = ['-prenom']
+    ordering = ['-grade']
+    ordering = ['-departement']
+    ordering = ['-password']
+    ordering = ['-Email']
+    ordering = ['-phone']
 
-   def __str__(self):
+
+    def __str__(self):
        return self.name
