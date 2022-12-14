ee8758f201af52ce76fa06fcf2dfa4ecb718cbe0
Yosra Omran <yosomran@gmail.com>
2022-12-14 03:03:50 +0100

    Comments Section

--- IsimmmoocFrontend-web/isimmmooc/src/Components/SectionComments/SectionComments.jsx
+++ IsimmmoocFrontend-web/isimmmooc/src/Components/SectionComments/SectionComments.jsx
@@ -8,11 +8,15 @@
 
 
 import * as React from 'react';
-
+import { useState } from 'react';
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
 import Paper from '@mui/material/Paper';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Typography from '@mui/material/Typography';
+
 
 
 /*
@@ -37,6 +41,8 @@ import styles from './../../Assets/Styles/style.module.scss'
  *                                Images                                |
  * ----------------------------------------------------------------------
  */
+import imageProfile from '../../Assets/Images/photo-profile.jpg';
+import commentIcon from '../../Assets/Images/comment-icom.png';
 
 function SectionComments(){
 
@@ -44,6 +50,10 @@ function SectionComments(){
    *                           Constants                                |
    * --------------------------------------------------------------------
    */
+  const [comments,setComments]=useState([
+    {txt:'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Earum voluptates quod dolorem aut voluptatum rerum, maiores eaque ipsa atque dolorum iusto perferendis est laboriosam qui itaque cupiditate. Obcaecati, perferendis unde?',
+     userName:'Yosra Omran'},
+  ])
   /* --------------------------------------------------------------------
    *                               Props                                |
    * --------------------------------------------------------------------
@@ -79,8 +89,10 @@ function SectionComments(){
   
   return (
     <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="lg" sx={{padding : '20px 0'}}>
+      <Container maxWidth="false" sx={{marginTop:'2%',
+        backgroundColor: '#f8f8ff',
+        width:"100%",}} 
+      >
         <Box sx={{ bgcolor: '#f8f8ff',
             height: '100%',
             padding: "20px 0",
@@ -101,14 +113,41 @@ function SectionComments(){
                         width: 300,
                         backgroundColor: (theme) =>
                           theme.palette.mode === 'dark' ? '#1A2027' : '#fff',
+                        border: '1px solid rgb(206, 194, 194)',
+                        boxShadow: 'rgba(0, 0, 0, 0.24) 0px 3px 8px',
+                        borderRadius:'15px',
                       }}
                       
-                     />
+                      >
+                      {comments.map((item)=>{
+                        return(
+                        <CardContent>
+                          <Grid container direction='row'>
+                          <img src={commentIcon} alt="commentIcon" />
+                          <Typography gutterBottom variant="h6" textAlign="center" fontWeight='bold' marginBottom="3%" marginLeft='10%' color='$SecondaryColor' component="div">
+                            {item.userName}
+                          </Typography>
+                          </Grid>
+                          <CardMedia 
+                            component="img"
+                            height="170"
+                            image={imageProfile}
+                            alt="userName photo"
+                            sx={{ borderRadius: '50%' }}
+                          />
+                          <Typography variant="body2" marginTop="3%" align='justify' fontFamily="Arial" fontSize='16'>
+                            {item.txt}
+                          </Typography>
+                        </CardContent>)
+                            
+                        })}
+                      </Paper>
                   </Grid>
                 ))}
               </Grid>
             </Grid>
           </Grid>
+       
         </Box>
       </Container>
     </React.Fragment>
