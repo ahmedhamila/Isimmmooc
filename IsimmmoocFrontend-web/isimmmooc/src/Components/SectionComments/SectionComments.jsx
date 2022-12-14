e8a9d8f5c6f32af3880970cfc8f490d686a8241e
Yosra Omran <yosomran@gmail.com>
2022-12-14 01:01:30 +0100

    Style SectionComments

--- IsimmmoocFrontend-web/isimmmooc/src/Components/SectionComments/SectionComments.jsx
+++ IsimmmoocFrontend-web/isimmmooc/src/Components/SectionComments/SectionComments.jsx
@@ -1,37 +1,22 @@
 import * as React from 'react';
-import CssBaseline from '@mui/material/CssBaseline';
+
 import Box from '@mui/material/Box';
 import Container from '@mui/material/Container';
 import Grid from '@mui/material/Grid';
-import { makeStyles } from "@material-ui/core/styles";
 import Paper from '@mui/material/Paper';
 
 import './SectionComments.scss'
 
-const useStyles = makeStyles((theme) => ({
-  container: {
-    padding: "20px 0",
-  },
-  containerHearder: {
-    textAlign:'center',
-    
-  },
-  h5: {
-    color:'#f5b70a',
-  }
-}))
 function SectionComments(){
-  const classes = useStyles();
+  
   return (
     <React.Fragment>
-      <CssBaseline />
-      <Container maxWidth="lg" className={classes.container}>
+      <Container maxWidth="lg" className='container-section'>
         <Box sx={{ bgcolor: '#f8f8ff',
             height: '100%',
             padding: "20px 0",
             position: "relative" }} >
-          <div className={classes.containerHearder}>
-            <h5 className={classes.h5}>Membership</h5>
+          <div style={{textAlign:'center'}}>
             <h1>What student says</h1>
             <p>A best and cheapest way of getting know learning to make a better tomorrow.</p>
           </div>    
